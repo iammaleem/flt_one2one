c277122df0e687e9e4236170318fe70770dc7b68
M.M Aleem <aleem@Aleems-MacBook-Pro.local>
2022-09-15 19:19:01 +0500

    JenkinsFile

--- lib/src/core/config/config.dart
+++ lib/src/core/config/config.dart
@@ -1,6 +1,6 @@
-var URL = "https://q-tenant.vdotok.dev";
+var URL = "https://tenant.vdotok.com";
 const version = "/API/v0/";
 
-const String auth_token = "3d9686b635b15b5bc2d19800407609fa";
+const String auth_token = "92f29c4596ed6572c40292ce43535e80";
 
-const String project_id = "115G1WZI";
+const String project_id = "11HUGUY";
